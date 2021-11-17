9607fcaa16f42e698664ed7d77d5c026d1b219c5
Rohit Nimkar <nehalnimkar@gmail.com>
2021-11-17 07:04:28 +0530

    :sparkles: add string to int

---
+++ string-to-int.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int atoi(char[]);
+
+int main()
+{
+    char s[] = "123";
+    printf("%d", atoi(s));
+
+    return 0;
+}
+
+/**
+ * Get the numberic value of string containing only digits
+ * */
+int atoi(char s[])
+{
+    printf("Input variable is : %s\n", s);
+    int i, n;
+    n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+    {
+        n = 10 * n + (s[i] - '0');
+    }
+    return n;
+}
